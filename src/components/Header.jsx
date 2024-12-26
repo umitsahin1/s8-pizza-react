09dfaa8a1d04cfde45b5c95bccba9f88b447fcc5
Umit Sahin <umitsahin21@gmail.com>
2024-12-26 11:35:41 +0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "./header.css";
+import logo2 from ../../Assets/Iteration-1-assets/logo.svg
 
 export default function Header() {
   return (
     <header className="header-css">
-      <img src="Assets/Iteration-1-assets/logo.svg" className="baslik" />
+      <img src={logo2} className="baslik" />
     </header>
   );
 }
